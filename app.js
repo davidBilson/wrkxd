0a7aa75fe300b3c1da142fd2ce60dc3e42868200
David Bilson <85068757+dlbi@users.noreply.github.com>
2022-07-15 01:05:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 let copyRightYear = document.querySelector('#copyright-year').textContent = new Date().getFullYear();
+
+let mintMins = document.getElementById('mintMins');
+let mintAdd = document.getElementById('mintAdd');
+let x = 0;
+
+mintAdd.addEventListener('click', function(){
+    let mintNmb = document.getElementById('mintNmb').value = ++x; 
+});
+mintMins.addEventListener('click', function(){
+    let mintNmb = document.getElementById('mintNmb').value = --x;
+});
+
+let popBtn = document.getElementById('mintBtn');
+let modal = document.getElementById('myModal');
+let span = document.getElementById('close');
+let clos = document.getElementById('closeBtn');
+
+popBtn.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = 'none';
+    }
+};
+
+span.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+clos.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
